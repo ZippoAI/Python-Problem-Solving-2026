919867372182194bb06bc416c81e71b16a1bed29
ZippoAI <iamrealzippo@gmail.com>
2026-08-19 03:29:44 +0530

    h

--- CodingBat.com/Warmup-1/1-sleep_in.py
+++ CodingBat.com/Warmup-1/1-sleep_in.py
@@ -1,15 +1,3 @@
-'''
-
-Q: 
-
-The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
-
-
-sleep_in(False, False) → True
-sleep_in(True, False) → False
-sleep_in(False, True) → True
-'''
-
 def sleep_in(weekday, vacation):
     if not weekday or vacation:
         return True
@@ -17,7 +5,4 @@ def sleep_in(weekday, vacation):
         return False
 
 
-
-
-
-  
+print(sleep_in(True, False))
